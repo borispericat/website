14d7eda16f815d89345a1740fd711b5beaa19fe9
borispericat <borispericat@gmail.com>
2023-03-03 22:47:44 +0100

    add express and clean code with eslint

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,20 +1,19 @@
-const path = require ('path')
+const path = require('path')
 
 const config = require('./webpack.config')
 
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
-const {merge} = require('webpack-merge')
-
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { merge } = require('webpack-merge')
 
 module.exports = merge(config, {
-    mode:'production',
+  mode: 'production',
 
-    output:{
-        path: path.join(__dirname, 'public')
-    },
+  output: {
+    path: path.join(__dirname, 'public')
+  },
 
-    plugins:[
-        new CleanWebpackPlugin()
-    ]
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
 
 })
