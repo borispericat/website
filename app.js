fdf44e2836176a28f01c0aa36fa588eb0464e372
borispericat <borispericat@gmail.com>
2023-03-04 17:02:35 +0100

    add pug template and update app.js with path

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
-// respond with "hello world" when a GET request is made to the homepage
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
 app.get('/', (req, res) => {
-  res.send('<h1>hello world</h1>')
+  res.render('pages/home')
 })
+
+app.get('/about', (req, res) => {
+  res.render('pages/about')
+})
+
+app.get('/collection', (req, res) => {
+  res.render('pages/collections')
+})
+
+app.get('/detail/:id', (req, res) => {
+  res.render('pages/detail')
+})
+
+
 
 app.listen(port, () => {
   console.log(`listening port: ${port}`)
